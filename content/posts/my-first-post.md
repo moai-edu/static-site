2f085adbed95b1b1f38c54d93dbacc09b929f200
Xin Li <bryt.xin.li@gmail.com>
2025-07-07 12:16:16 +0800

    SETUP: init

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,10 @@
++++
+date = '2025-07-07T09:38:55+08:00'
+draft = true
+title = 'My First Post'
++++
+## Introduction
+
+This is **bold** text, and this is *emphasized* text.
+
+Visit the [Hugo](https://gohugo.io) website!
---
+++ sst.config.ts
@@ -0,0 +1,19 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+    app(input) {
+        return {
+            name: "static-site",
+            home: "aws",
+            removal: "remove",
+        };
+    },
+    async run() {
+        const infra = await import("./infra");
+        return {
+            staticSiteUrl: infra.static_site.url,
+            staticSiteS3BucketName: infra.static_site.nodes.assets?.name,
+        };
+        // 注意：这里一定记得同步更新sst-end.d.ts文件，否则编译可能失败。
+    },
+});
